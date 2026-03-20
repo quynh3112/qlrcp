e7bf3361f98d1194efc76d7301fbed8ff9999e54
quynh <nhuquynh20053112@gmail.com>
2026-03-20 22:46:16 +0700

    xong

---
+++ controllers/cinemaController.php
@@ -0,0 +1,112 @@
+<?php
+include "../models/Cinema.php";
+header("Content-Type: application/json");
+$cinema=new Cinema();
+$method=$_SERVER['REQUEST_METHOD'];
+switch($method){
+    case "POST":
+        $data=json_decode(file_get_contents("php://input"),true);
+        $name=$data['name'] ?? null;
+        $location=$data['location']?? null;
+        if(!$name || !$location){
+            echo json_encode([
+                "status"=>false,
+                "message"=>"Không được bỏ trống!"
+            ]);
+            exit;
+        }
+        $result=$cinema->create($name,$location);
+        echo json_encode([
+            "status"=>$result,
+            "message"=>$result ? "Thêm chi nhánh mới thành công!":"Thêm thất bại!"
+        ]);
+        break;
+    case "DELETE":
+        $data=json_decode(file_get_contents("php://input"),true);
+        $id=$data['id'];
+        if(!$id){
+            echo json_encode([
+                "status"=>false,
+                "message"=>"Thiếu id"
+            ]);
+            exit;
+
+        }
+        $found=$cinema->find($id);
+        if($found->num_rows<=0){
+            echo json_encode([
+                "status"=>false,
+                "message"=> "ID không tồn tại"
+            ]);
+            exit;
+
+        }
+        $result=$cinema->delete($id);
+        echo json_encode([
+            "status"=>$result,
+            "message"=>$result? "Xóa thành công!":"Xóa thất bại!"
+        ]);
+        break;
+    case "PUT":
+        $data=json_decode(file_get_contents("php://input"),true);
+        $name=$data['name'] ?? null;
+        $location=$data['location']?? null;
+        $id=$data['id'] ??null;
+        if(!$id){
+            echo json_encode([
+                "status"=>false,
+                "message"=>"Thiếu id"
+            ]);
+            exit;
+
+        }
+         $found=$cinema->find($id);
+        if($found->num_rows<=0){
+            echo json_encode([
+                "status"=>false,
+                "message"=> "ID không tồn tại"
+            ]);
+            exit;
+
+        }
+        $result=$cinema->edit($id,$name,$location);
+        echo json_encode([
+            "status"=>$result,
+            "message"=>$result? "Sửa thành công!":"Sửa thất bại!"
+        ]);
+        break;
+    case "GET":
+       
+    $id = $_GET['id'] ?? null; 
+
+    if ($id) {
+        $result = $cinema->find($id);
+
+        if ($result->num_rows <= 0) {
+            echo json_encode([
+                "status" => false,
+                "message" => "Không tìm thấy cinema"
+            ]);
+            exit;
+        }
+
+        echo json_encode($result->fetch_assoc());
+    } else {
+        $result = $cinema->findAll();
+
+        $list = [];
+        while ($row = $result->fetch_assoc()) {
+            $list[] = $row;
+        }
+
+        echo json_encode($list);
+    }
+    break;
+
+
+
+    
+
+}
+
+?>
---
+++ models/Cinema.php
@@ -0,0 +1,39 @@
+<?php
+include "../config/db.php";
+class Cinema{
+    public function create($name,$location){
+        global $conn;
+        $sql="INSERT INTO Cinemas (name,location) VALUES ('$name','$location')";
+        return $conn->query($sql);
+
+    }
+    public function delete($id){
+        global $conn;
+        $sql="DELETE FROM Cinemas WHERE cinema_id= '$id'";
+        return $sql->query($sql);
+    }
+    public function edit($id,$name,$location){
+        global $conn;
+        $sql="UPDATE Cinemas Set name='$name', location='$location' Where cinema_id='$id'";
+        return $conn->query($sql);
+      
+    }
+    public function findAll(){
+        global $conn;
+        $sql="SELECT * FROM Cinemas";
+        return $conn->query($sql);
+    }
+    public function find($id){
+        global $conn;
+        $sql="Select * FROM Cinemas WHERE cinema_id='$id'";
+        return $conn->query($sql);
+
+    }
+    public function findAllCinema(){
+        global $conn;
+        $sql="Select * from Cinemas ";
+        return $conn->query($sql);
+    }
+    
+}
+?>
