3c8cc5b58c53c3b9054a474528c29fea527d58f4
quynh <nhuquynh20053112@gmail.com>
2026-03-20 09:12:13 +0700

    .

---
+++ models/User.php
@@ -0,0 +1,24 @@
+<?php
+include "../config/db.php";
+class User{
+   public function create($username, $password, $email, $phone, $role) {
+    global $conn;
+
+    if (empty($role)) {
+        $role = "customer";
+    }
+
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO Users (username, password, email, phone, role) 
+            VALUES ('$username', '$hash', '$email', '$phone', '$role')";
+
+    return $conn->query($sql);
+}
+    public function delete($id){
+        global $conn;
+        $sql="DELETE FROM Users WHERE user_id= '$id' ";
+        return $conn->query($sql);
+    }
+}
+?>
